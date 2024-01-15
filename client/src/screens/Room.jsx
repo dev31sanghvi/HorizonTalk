68be580267b5df789bb607645c324c6d23b60185
Dev Sanghvi <devsanghvi06@gmail.com>
2024-01-15 20:11:17 +0530

    Screen Sharing feature add

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -8,6 +8,51 @@ const RoomPage = () => {
   const [myStream, setMyStream] = useState();
   const [remoteStream, setremoteStream] = useState();
   const [isAudioMuted, setIsAudioMuted] = useState(false);
+  const [isScreenSharing, setIsScreenSharing] = useState(false);
+
+  //function for starting screen sharing
+  const startScreenSharing = async () => {
+    try {
+      const screenStream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+      });
+      // this is for user's screen
+      const videoTrack = screenStream.getVideoTracks()[0];
+      const audioTrack = myStream.getAudioTracks()[0];
+      //to remove the existing audio track
+      myStream.removeTrack(myStream.getVideoTracks()[0]);
+      // this will add the video tracks that we got from myStream
+      myStream.addTrack(videoTrack);
+
+      // this will send the updated stream to peer
+      sendStreams();
+
+      // to display the user screen
+      setMyStream(myStream);
+      setIsScreenSharing(true);
+    } catch (error) {
+      console.error("Sorry ! Error Occured:", error);
+
+    }
+  };
+
+  // function for stopping live Stream
+  const stopScreenSharing = () => {
+    // restoring video track from myStream
+    const originalVideoTrack = myStream.getVideoTracks()[0];
+  const screenStreamVideoTrack = myStream.getTracks()[0];
+  myStream.removeTrack(screenStreamVideoTrack);
+  myStream.addTrack(originalVideoTrack);
+
+
+    // this will send the updated stream to peer
+    sendStreams();
+
+    setMyStream(myStream);
+    setIsScreenSharing(false);
+
+
+  };
 
   // user joined function
   const handleUserJoined = useCallback(({ email, id }) => {
@@ -47,11 +92,11 @@ const RoomPage = () => {
 
   );
 
-  const sendStreams=useCallback(()=>{
+  const sendStreams = useCallback(() => {
     for (const track of myStream.getTracks()) {
       peer.peer.addTrack(track, myStream);
     }
-  },[myStream])
+  }, [myStream])
   // function to accept call
   const handleCallAccepted = useCallback(
     ({ from, ans }) => {
@@ -98,15 +143,15 @@ const RoomPage = () => {
       // taking our own remote stream
       const remoteStream = ev.streams;
       console.log("Got Tracks");
-//streams are basically an array
+      //streams are basically an array
       setremoteStream(remoteStream[0]);
     });
   }, [])
 
   //Audio mute button :
-  const toggleAudioMute=()=>{
-    if(myStream){
-      const audioTracks=myStream.getAudioTracks();
+  const toggleAudioMute = () => {
+    if (myStream) {
+      const audioTracks = myStream.getAudioTracks();
       audioTracks.forEach((track) => {
         track.enabled = !track.enabled;
       });
@@ -132,11 +177,13 @@ const RoomPage = () => {
       socket.off("peer:nego:final", handleNegoNeedFinal);
     };
   }, [socket, handleUserJoined, handleIncommingCall, handleCallAccepted, handleNegoNeedIncomming, handleNegoNeedFinal]);
+
+
   return (
     <div>
       <h1>Room</h1>
       <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
-      {myStream && <button onClick={sendStreams}>Send stream</button> }
+      {myStream && <button onClick={sendStreams}>Send stream</button>}
       {remoteSocketId && <button onClick={handleCallUser}>CALL</button>}
       {myStream && (
         <>
@@ -149,9 +196,15 @@ const RoomPage = () => {
             width="300px"
             url={myStream}
           /> */}
-           <h1>My video</h1>
-    <video autoPlay muted ref={(ref) => (ref ? (ref.srcObject = myStream) : null)} />
-    <button onClick={toggleAudioMute}>{isAudioMuted ? "Unmute" : "Mute"}</button>
+          <h1>My video</h1>
+          <video autoPlay muted ref={(ref) => (ref ? (ref.srcObject = myStream) : null)} />
+          <button onClick={toggleAudioMute}>{isAudioMuted ? "Unmute" : "Mute"}</button>
+          {/* screen sharing part */}
+          {isScreenSharing ? (
+            <button onClick={stopScreenSharing}>Stop Screen Sharing</button>
+          ) : (
+            <button onClick={startScreenSharing}>Start Screen Sharing</button>
+          )}
         </>
       )}
       {remoteStream && (
@@ -165,9 +218,9 @@ const RoomPage = () => {
             width="300px"
             url={remoteStream}
           /> */}
-           <h1>Remote Stream</h1>
-    <video autoPlay muted ref={(ref) => (ref ? (ref.srcObject = remoteStream) : null)} />
-    <audio autoPlay ref={(ref) => (ref ? (ref.srcObject = remoteStream) : null)} />
+          <h1>Remote Stream</h1>
+          <video autoPlay muted ref={(ref) => (ref ? (ref.srcObject = remoteStream) : null)} />
+          <audio autoPlay ref={(ref) => (ref ? (ref.srcObject = remoteStream) : null)} />
         </>
       )}
     </div>
